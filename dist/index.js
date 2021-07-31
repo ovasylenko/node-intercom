a270cf21a6b98cd139c83dea8e730f96d628dcde
GlebShulga <shulga_gleb@hotmail.com>
2021-07-31 17:20:00 +1000

    add contact interfaces

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
-    Test: 'test12345'
+    Test: 'test123452'
 };
 //# sourceMappingURL=index.js.map
--- dist/interfaces/contact.js
+++ dist/interfaces/contact.js
@@ -1,6 +1,3 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    contact: "test123456",
-};
 //# sourceMappingURL=contact.js.map
